b8235929f5f1425c51a7fa6db18542cf5ad4fe29
Ali <ali.rac200@gmail.com>
2016-01-07 00:00:00 +0000

    C3 - Added javadocs to square and cube

--- Foo.java
+++ Foo.java
@@ -3,11 +3,23 @@ package test;
 
 public class Foo
 {
+    /**
+     * Given a number, this method multiplies it to itself
+     * and returns it.
+     *
+     * E.g, given 2, it will return 2 x 2 = 4
+     */
     public long square(long number)
     {
         return number * number + 1 + 1;
     }
 
+    /**
+     * Given a number, this method multiplies that number to itself 3 times, and
+     * returns it.
+     *
+     * E.g, given 2, it will return 2 x 2 x 2 = 8
+     */
     public long cube(long number)
     {
         return number * number * number + 1;
